cb9df340ba655f1187fcfd15065c1c3fb5f8d43c
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-11-13 11:21:42 -0700

    Downgraded Inquirer package to clear error on require. Set up basic README file creation/modification.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+//require('./generateMarkdown.js');
+
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'enterName',
+    },
+]).then((response) =>{
+
+    /* .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!') */
+
+    const readmeMD = '# This is a README file.';
+
+    fs.writeFile('README.md', `${readmeMD}`, (err)=>{
+        return console.log("done!");
+    } );
+
+})
