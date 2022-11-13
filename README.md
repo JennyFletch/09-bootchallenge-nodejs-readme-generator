f92bfe936c7f71c0b8d8ced98036fdee0237dbfd
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-11-13 11:26:11 -0700

    Included generateMarkdown.js and added dynamic content to README.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# This is a README file.
+# This is Jen's README file.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-//require('./generateMarkdown.js');
+require('./generateMarkdown.js');
 
 
 inquirer.prompt([
@@ -11,12 +11,14 @@ inquirer.prompt([
     },
 ]).then((response) =>{
 
+    const {enterName} = response;
+    
     /* .then((response) =>
     response.confirm === response.password
       ? console.log('Success!')
       : console.log('You forgot your password already?!') */
 
-    const readmeMD = '# This is a README file.';
+    const readmeMD = `# This is ${enterName}'s README file.`;
 
     fs.writeFile('README.md', `${readmeMD}`, (err)=>{
         return console.log("done!");
